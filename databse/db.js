420192915a7b787ef403d8a0d6be01600d3f7a81
RajithaKWG <rkrishan894@gmail.com>
2025-02-01 16:54:03 +0530

    Implemented codes for connect with database

--- databse/db.js
+++ databse/db.js
@@ -1,3 +1,15 @@
 
 const mongoose = require('mongoose');
 
+const connectToDB = async() => {
+    try{
+        await mongoose.connect('mongodb+srv://krishanjesin:rkw1234@cluster0.8uons.mongodb.net/');
+        console.log('MongoDB is connected successfully!')
+    }catch{
+        console.error('Mongo connection failed!', error);
+        process.exit(1); 
+    }
+};
+
+module.exports = connectToDB;
+
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
+const connectToDB = require('./databse/db')
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
 //connet to database
+connectToDB();
+
+//middleware -> express.json()
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is now running on port ${PORT}`);
+});
