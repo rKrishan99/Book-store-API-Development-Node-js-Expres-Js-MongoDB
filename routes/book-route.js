4c22c507c5072aae5a2a01a2b5eb106b81805163
RajithaKWG <rkrishan894@gmail.com>
2025-02-01 17:25:35 +0530

    Implemented routers

---
+++ routes/book-route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+//create express router
+const router = express.Router();
+
+//all routers that are related to books only
+router.get('/get',);
+router.get('/get/:id',);
+router.post('/add',);
+router.put('/upadate/:id',);
+router.delete('/delete/:id',);
